4c0608069ed1b9d1026a64b73321c1c2f903900c
fahidmeharcrypto <fahidmehar91@gmail.com>
2025-04-26 01:08:40 +0300

    Requirements
    flask

---
+++ signal_generator.py
@@ -0,0 +1,5 @@
+def generate_signals():
+    return [
+        {"pair": "BTC/USDT", "direction": "Long", "entry": "64000", "stop_loss": "63000", "take_profit": "66000"},
+        {"pair": "ETH/USDT", "direction": "Short", "entry": "3200", "stop_loss": "3300", "take_profit": "3000"},
+    ]
