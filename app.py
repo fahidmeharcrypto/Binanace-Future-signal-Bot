8a815c2cfa621f59fa9db5351459adb23f00bc33
fahidmeharcrypto <fahidmehar91@gmail.com>
2025-04-26 14:26:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask, render_template
-from signal_generator import generate_signals
-import time
+from signal_generator import signal_generator
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    signals = generate_signals()
-    timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
-    return render_template('index.html', signals=signals, timestamp=timestamp)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def index():
+    signal = signal_generator()
+    return render_template('index.html', signal=signal)
